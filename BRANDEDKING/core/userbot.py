279509fc7fcf9e984343ca20fe70ab21743b526d
jack77force <159509153+jack77force@users.noreply.github.com>
2024-04-04 23:27:24 +0530

    Update userbot.py

--- BRANDEDKING/core/userbot.py
+++ BRANDEDKING/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="BRANDEDAss1",
+            name="TOXICAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="BRANDEDAss2",
+            name="TOXICAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="BRANDEDAss3",
+            name="TOXICAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="BRANDEDAss4",
+            name="TOXICAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="BRANDEDAss5",
+            name="TOXICAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.one.join_chat("toxic_mod_vip")
+                await self.one.join_chat("toxic_mod_vip")
+                await self.one.join_chat("TOXIC_WORLD8")
             except:
                 pass
             assistants.append(1)
@@ -73,9 +73,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.two.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.two.join_chat("toxic_mod_vip")
+                await self.two.join_chat("toxic_mod_vip")
+                await self.one.join_chat("TOXIC_WORLD8")
             except:
                 pass
             assistants.append(2)
@@ -95,9 +95,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
-                await self.three.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.three.join_chat("toxic_mod_vip")
+                await self.three.join_chat("toxic_mod_vip")
+                await self.one.join_chat("TOXIC_WORLD8")
             except:
                 pass
             assistants.append(3)
@@ -117,9 +117,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
-                await self.four.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.four.join_chat("toxic_mod_vip")
+                await self.four.join_chat("toxic_mod_vip")
+                await self.one.join_chat("TOXIC_WORLD8")
             except:
                 pass
             assistants.append(4)
@@ -139,9 +139,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
-                await self.five.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.five.join_chat("toxic_mod_vip")
+                await self.five.join_chat("toxic_mod_vip")
+                await self.one.join_chat("TOXIC_WORLD8")
             except:
                 pass
             assistants.append(5)
